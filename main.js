3e5830fa2cd2890f46c5d8f0c83ba5683587bd12
OluwaDAN <danielloluwatosin@gmail.com>
2020-09-25 02:27:35 +0100

    card detector

---
+++ main.js
@@ -0,0 +1,37 @@
+let confirmbutton = document.getElementById("mybtn");
+
+confirmbutton.addEventListener("click", buttonclick);
+let master = document.getElementById("mastercard");
+let visa = document.getElementById("visacard");
+let error = document.getElementById("error")
+
+master.style.display="none";
+visa.style.display="none";
+error.style.display="none";
+
+ function buttonclick(){
+        let input = document.getElementById("inputNumber").value;
+
+        if(input.length >= 16){  
+        if(input.startsWith(5)){
+         console.log("It's a Mastercard!");
+         master.style.display ="block";
+         visa.style.display ="none";
+         error.style.display = "none"
+        }
+        else if(input.startsWith (4)){
+         console.log(`It's a Visa Card!`);
+         visa.style.display ="block";
+         master.style.display ="none";
+         error.style.display = "none"
+        }
+
+    }
+        else {
+         console.log(`error`);
+         error.style.display = "block"
+         visa.style.display ="none";
+         master.style.display ="none";
+        }
+     
+    }
